1b9b6a2f5b8a2ead51973f21b943de13308fb994
Jason Nicolas Carvajal Perez <doble_papi@MacBook-Pro.local>
2022-04-06 10:46:22 +0200

    sidebar structure

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,19 @@
-import React from 'react'
-import logo from './logo.svg'
-import { FaTimes } from 'react-icons/fa'
-import { social, links } from './data'
+import React from "react";
+import logo from "./logo.svg";
+import { FaTimes } from "react-icons/fa";
+import { social, links } from "./data";
 
 const Sidebar = () => {
-  return <h2>sidebar</h2>
-}
+  return (
+    <section className="sidebar">
+      <div className="sidebar-header">
+        <button className="close-btn">
+          <FaTimes />
+        </button>
+        <div className="logo">{logo}</div>
+      </div>
+    </section>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
